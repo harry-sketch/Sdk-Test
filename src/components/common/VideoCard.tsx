fba940aad52c19d448fbf5aa86e454bdfab77a19
harry-sketch <harshbisht90@gmail.com>
2024-01-04 17:22:06 +0530

    add: fiber

--- src/components/common/VideoCard.tsx
+++ src/components/common/VideoCard.tsx
@@ -1,4 +1,10 @@
+import {
+  FaceLandmarker,
+  FaceLandmarkerOptions,
+  FilesetResolver,
+} from "@mediapipe/tasks-vision";
 import React, { useEffect, useRef } from "react";
+import { Euler, Matrix4 } from "three";
 
 interface Props {
   stream?: MediaStream;
@@ -7,12 +13,36 @@ interface Props {
   objectCover?: boolean;
 }
 
+let faceLandmarker: FaceLandmarker;
+let lastVideoTime = -1;
+
+const options: FaceLandmarkerOptions = {
+  baseOptions: {
+    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+    delegate: "GPU",
+  },
+  numFaces: 1,
+  runningMode: "VIDEO",
+  outputFaceBlendshapes: true,
+  outputFacialTransformationMatrixes: true,
+};
+
 const VideoCard: React.FC<Props> = ({ stream, isMe }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
 
   const dependencies = isMe ? [stream] : [];
 
-  useEffect(() => {
+  const setup = async () => {
+    if (!videoRef.current) return;
+
+    const filesetResolver = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+    );
+    faceLandmarker = await FaceLandmarker.createFromOptions(
+      filesetResolver,
+      options
+    );
+
     const videoObj = videoRef.current;
 
     if (videoObj && stream) {
@@ -30,6 +60,40 @@ const VideoCard: React.FC<Props> = ({ stream, isMe }) => {
         console.error("videoCard() | Error is hapenning...");
       };
     }
+    // videoRef.current.addEventListener("loadeddata", predict);
+  };
+
+  // const predict = async () => {
+  //   if (!videoRef.current) return;
+
+  //   let nowInMs = Date.now();
+
+  //   if (lastVideoTime !== videoRef.current.currentTime) {
+  //     lastVideoTime = videoRef.current.currentTime;
+  //     const faceLandmarkerResult = faceLandmarker.detectForVideo(
+  //       videoRef.current,
+  //       nowInMs
+  //     );
+
+  //     if (
+  //       faceLandmarkerResult.faceBlendshapes &&
+  //       faceLandmarkerResult.faceBlendshapes.length > 0 &&
+  //       faceLandmarkerResult.faceBlendshapes[0].categories
+  //     ) {
+  //       blendshapes = faceLandmarkerResult.faceBlendshapes[0].categories;
+
+  //       const matrix = new Matrix4().fromArray(
+  //         faceLandmarkerResult.facialTransformationMatrixes![0].data
+  //       );
+  //       rotation = new Euler().setFromRotationMatrix(matrix);
+  //     }
+  //   }
+
+  //   window.requestAnimationFrame(predict);
+  // };
+
+  useEffect(() => {
+    setup();
   }, dependencies);
 
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,59 @@
 import View from "@/components/View/View";
-import { useRoom } from "@huddle01/react/hooks";
-import { AccessToken, Role } from "@huddle01/server-sdk/auth";
+
+import { useGLTF } from "@react-three/drei";
+import { useFrame, useGraph } from "@react-three/fiber";
+import { useEffect } from "react";
+import { Euler } from "three";
 
 type Props = {
   token: string;
 };
 
+let blendshapes: any[] = [];
+let rotation: Euler;
+let headMesh: any[] = [];
+
+function Avatar({ url }: { url: string }) {
+  const { scene } = useGLTF(url);
+  const { nodes } = useGraph(scene);
+
+  useEffect(() => {
+    if (nodes.Wolf3D_Head) headMesh.push(nodes.Wolf3D_Head);
+    if (nodes.Wolf3D_Teeth) headMesh.push(nodes.Wolf3D_Teeth);
+    if (nodes.Wolf3D_Beard) headMesh.push(nodes.Wolf3D_Beard);
+    if (nodes.Wolf3D_Avatar) headMesh.push(nodes.Wolf3D_Avatar);
+    if (nodes.Wolf3D_Head_Custom) headMesh.push(nodes.Wolf3D_Head_Custom);
+  }, [nodes, url]);
+
+  useFrame(() => {
+    if (blendshapes.length > 0) {
+      blendshapes.forEach((element) => {
+        headMesh.forEach((mesh) => {
+          let index = mesh.morphTargetDictionary[element.categoryName];
+          if (index >= 0) {
+            mesh.morphTargetInfluences[index] = element.score;
+          }
+        });
+      });
+
+      nodes.Head.rotation.set(rotation.x, rotation.y, rotation.z);
+      nodes.Neck.rotation.set(
+        rotation.x / 5 + 0.3,
+        rotation.y / 5,
+        rotation.z / 5
+      );
+      nodes.Spine2.rotation.set(
+        rotation.x / 10,
+        rotation.y / 10,
+        rotation.z / 10
+      );
+    }
+  });
+
+  return <primitive object={scene} position={[0, -1.75, 3]} />;
+}
+
 export default function Home({ token }: Props) {
-  const { joinRoom, state } = useRoom();
-
-  const handleJoinRoom = async () => {
-    await joinRoom({
-      roomId: "fge-bxdp-hwr",
-      token,
-    });
-  };
-
   return (
     <main className="">
       <View />
